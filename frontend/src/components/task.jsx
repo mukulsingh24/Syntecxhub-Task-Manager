80e59cc791385f4809e0791fa8480ffa20c1efec
mukulsingh24 <rmks1004@gmail.com>
2025-11-10 00:04:41 +0530

    delete button done

--- frontend/src/components/task.jsx
+++ frontend/src/components/task.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 
 function Task(){
     const[task,setTask] = useState("");
-    const[taskList,setTaskList] = useState("");
+    const[taskList,setTaskList] = useState([]);
     const handleSubmit=()=>{
         setTaskList([...taskList,task])
         setTask("")
     } 
+    const handleDelete =(itemIndex) =>{
+        const newList = taskList.filter((_,index)=>{
+            return index != itemIndex
+        })
+        setTaskList(newList)
+    }
     return(
         <div>
             <h1>Task Manager</h1>
@@ -21,6 +27,7 @@ function Task(){
                 {taskList.map((item,index) =>(
                 <li key={index}>{item}</li>))}
             </ul>
+            <button onClick={()=>{handleDelete(index)}}>Delete Task</button>
         </div>
     )
 }

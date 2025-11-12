4afe53a8ef13d3a209fe3f29aab9e69eff4a6c37
mukulsingh24 <rmks1004@gmail.com>
2025-11-12 08:29:20 +0530

    db installed

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,16 @@ const multer = require('multer')
 const mongoose = require('mongoose')
 
 const app = express();
-app.use()
+app.use(cors())
 const PORT = 5000;
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/myTaskDatabase')
+.then(() => console.log("Database Connected Successfully"))
+.catch(() => console.log("Error Connecing Database"))
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`)
+})
 
 

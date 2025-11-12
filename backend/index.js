3b0181d303c8bb4ecd2fc2ec869acc8145d18325
mukulsingh24 <rmks1004@gmail.com>
2025-11-12 08:19:15 +0530

    installed required frameworks for backend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+const mongoose = require('mongoose')
+
+const app = express();
+app.use()
+const PORT = 5000;
+
+

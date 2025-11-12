0de87e780377ca2aec3ab5bb6fecec77cd0f68e1
mukulsingh24 <rmks1004@gmail.com>
2025-11-12 08:30:57 +0530

    some fixes

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose')
 
 const app = express();
 app.use(cors())
-const PORT = 5000;
 app.use(express.json())
 
 mongoose.connect('mongodb://127.0.0.1:27017/myTaskDatabase')
 .then(() => console.log("Database Connected Successfully"))
-.catch(() => console.log("Error Connecing Database"))
+.catch((err) => console.log("Error Connecing Database",err))
 
+const PORT = 5000;
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`)
 })

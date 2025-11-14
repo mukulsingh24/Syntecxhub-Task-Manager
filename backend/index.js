a2e5cb3e178a682e8e3b1f7f9c91241edfcf9495
mukulsingh24 <rmks1004@gmail.com>
2025-11-14 19:57:44 +0530

    db connected successfully

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,16 @@ const mongoose = require('mongoose')
 const app = express();
 app.use(cors())
 app.use(express.json())
-
-
 const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log('Database Connected Successfully')
+})
+.catch((error)=>{
+    console.log('Error Connecting Database:', error);
+})
+
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`)
 })

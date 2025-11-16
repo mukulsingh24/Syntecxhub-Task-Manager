6b1fc860db4a6f7d0e129b1d1da6495a40c91b73
mukulsingh24 <rmks1004@gmail.com>
2025-11-16 14:51:52 +0530

    backend auth done

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router();
 const authController = require("../controllers/authController")
 
-router.post('/register',authController);
+router.post('/register',authController.Register);
+router.post('/login',authController.Login);
 
 module.exports = router;

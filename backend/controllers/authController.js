cebfcdac3ab354f3ebe2f13f782d1d5c029061ba
mukulsingh24 <rmks1004@gmail.com>
2025-11-16 15:07:31 +0530

    jwt and auth done for the user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,31 +1,36 @@
 const User = require("../models/User");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Register = async (req, res) => {
-    // 200 == successfull
-    if(await User.findOne({email: req.body.email})){
-        res.status(200).json({message:"User Exists"})
-    }
-    else{
-        const newUser = new User({email:req.body.email,password:req.body.password})
-        await newUser.save()
-        res.status(200).json({message:"New user"})
-
-    }    
+  // 200 == successfull
+  if (await User.findOne({ email: req.body.email })) {
+    res.status(200).json({ message: "User Exists" });
+  } else {
+    const newUser = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser.save();
+    res.status(200).json({ message: "New user" });
+  }
 };
 
-const Login = async(req,res) =>{
-    const user = await User.findOne({email:req.body.email})
-    if(user){
-        if(await bcrypt.compare(req.body.password,user.password)){
-        res.status(200).json({message:"Login Successfully"})
+const Login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .status(200)
+        .json({ token, userId: user._id, message: "Login Successful" });
+    } else {
+      res.status(200).json({ message: "Invalid Credentails" });
     }
-    else{
-        res.status(200).json({message:"Invalid Credentails"})
-    }
-}
-    else{
-        res.status(200).json({message:"User Doesnt Exists"})
-    }
-}
+  } else {
+    res.status(200).json({ message: "User Doesnt Exists" });
+  }
+};
 
-module.exports = {Register,Login};
+module.exports = { Register, Login };
